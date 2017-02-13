a5b7af47345708e470d72afb5774e47b3dc00d7f
clenk <clenk@mitre.org>
2017-02-13 10:06:20 -0500

    Fix anchor link placement for headers with images

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
-
-$(function() {
-  return $("h2, h3, h4, h5, h6").each(function(i, el) {
+$(document).ready(function($) {
+  $("h2, h3, h4, h5, h6").each(function(i, el) {
     var $el, icon, id;
     $el = $(el);
     id = $el.attr('id');
     icon = '<i class="glyphicon glyphicon-link"></i>';
     if (id) {
-      return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+      anchor = $("<a />").addClass("header-link").attr("href", "#" + id).html(icon);
+      if ($el.has('img').length) {
+        anchor.css('top', el.offsetTop + 23 + 'px');
+      }
+      $el.prepend(anchor);
     }
   });
 });
